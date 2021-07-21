fb751cae561547e4debfb71633428461e6f23ef8
codingtrain1515 <85920226+codingtrain1515@users.noreply.github.com>
2021-07-20 23:51:03 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,20 +7,25 @@ var engine, world;
 var canvas;
 var palyer, playerBase;
 var computer, computerBase;
-
+var compArrow;
+var computerArcher
 var arrow;
+
+var plArrows = [];
+var CmpArrows = [];
 
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth,windowHeight);
 
   engine = Engine.create();
   world = engine.world;
 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
   player = new Player(285, playerBase.body.position.y - 153, 50, 180);
- 
+  playerArcher = new PlayerArcher(345, player.body.position.y - 25, 120, 120);
   //Create Player Archer Object
+  
 
   computerBase = new ComputerBase(
     width - 300,
@@ -39,10 +44,9 @@ function setup() {
     computerBase.body.position.y - 180,
     120,
     120
-  );
-  
-  //Create an arrow Object
-  
+  ); 
+
+ //Create an arrow Object
   
 }
 
@@ -57,7 +61,6 @@ function draw() {
   textSize(40);
   text("EPIC ARCHERY", width / 2, 100);
 
- 
   playerBase.display();
   player.display();
   
@@ -67,16 +70,47 @@ function draw() {
   
   playerArcher.display();
   computerArcher.display()
-
-
-  //Display arrow();
   
-  //if Space (32) key is pressed call shoot function of playerArrow
+//Playerarrow.display();
+ 
+}
+function keyPressed() {
+
   if(keyCode === 32){
-    //Call shoot() function and pass angle of playerArcher
+    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
+    var posX = computerArcher.body.position.x;
+    var posY = computerArcher.body.position.y;
+    var angle = computerArcher.body.angle+PI/2;
 
+    arrow.display()
 
+    var arrow = new Computerarrow(width - 340,player.body.position.y - 5,80,20)
+    Matter.Body.setAngle(arrow.body, angle);
+    CmpArrows.push(arrow);
+  
   }
+}
+
+function keyReleased () {
+
+  if(keyCode === 32){
+    //call shoot() function for each arrow in an array playerArrows
+    if (playerArrow.length) {
+      var angle = computerArcher.body.angle+PI/2;
+      CmpArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+
+
+}
+
+function showArrows(index, arrows) {
+  arrows[index].display();
+  
+    
+  
+ 
+
 }
 
 
